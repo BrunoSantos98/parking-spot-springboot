2b82301c5a761b192b3d8ea14bd3a57fe6a314a5
Bruno Santos <bruno4t@hotmail.com>
2022-12-12 11:21:33 -0300

    Criação do CRUD inicial

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# workshop-springboot3-jpa
+
+> ℹ️ Detalhes: Aplicação Back-end feita em java + Spring boot + sql, onde se simula uma API de CRUD de um condominio com vagas de estacionamento de veículo demarcadas por apartamento. O objetivo desta API é de aprender Spring, notações JPA e organização de projeto.
+
+- 🚧 melhorias a serem feitas 🚧 :
+
+  - [ ]  Criar uma entidade carro e separar da entidade parkingSpot.
+  - [ ]  Fazer a relação 1:1 entre as entidades carro e parkingSpot.
+  - [ ]  inserir parâmetros e filtros na busca por metodo GET(all).
+  - [ ]  Criar excessões e tratar possíveis erros da aplicação.
+  - [ ]  Criar metodos especificos de validação retirando os if-elses dos metodos do controller.
+
+## 💻 Pré Requisitos 💻:
+
+- Sistema operacional: Qualquer Distribuição Linux, Windows e Mac OS
+
+- Instalação: é necessario ter o java 18(jre e sdk) instalado em sua maquina para testar e qualquer IDE java ou um editor de sua preferencia que consiga
+  compilar java. O banco de dados utilizado é o H2, então caso queira popular um banco e fazer com seus casos de teste você deve atualizar as informações de propriedades do banco de dados. Você tambem deve ter o Maven instalado na sua maquina.
+
+## 📃 Como usar:
+
+1. Faça o clone do projeto
+
+2. abra na sua IDE ou editor de preferencia
+
+3. abra o arquivo "pom.xml" e verifique se falta instalar alguma depencia, caso seja necessario faça o Download das dependencias faltantes para o bom funcionamento do projeto
+
+4. Teste como quiser.
---
+++ src/main/java/com/bruno/parkingspot/controller/ParkingSpotController.java
@@ -0,0 +1,87 @@
+package com.bruno.parkingspot.controller;
+
+import com.bruno.parkingspot.dtos.ParkingSpotDTO;
+import com.bruno.parkingspot.models.ParkingSpotModel;
+import com.bruno.parkingspot.services.ParkingSpotService;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.event.HyperlinkEvent;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*",maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+
+    @Autowired
+    ParkingSpotService parkingSpotService;
+
+    @PostMapping
+    public ResponseEntity<Object> savingParkingSpot(@RequestBody @Valid ParkingSpotDTO parkingSpotDTO){
+        if(parkingSpotService.existsByLicensePlateCar(parkingSpotDTO.getLicensePlateCar())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: License Plate Car is already in use!");
+        }
+        if(parkingSpotService.existsByParkingSpotNumber(parkingSpotDTO.getParkingSpotNumber())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot is already in use!");
+        }
+        if(parkingSpotService.existsByApartmentAndBlock(parkingSpotDTO.getApartment(), parkingSpotDTO.getBlock())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot already registered for this apartment/block!");
+        }
+        var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDTO, parkingSpotModel);
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpots(@PageableDefault(page=0, size=10,sort = "id",direction = Sort.Direction.ASC)Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneParkingSpot(@PathVariable (value="id" ) UUID id){
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if(parkingSpotModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("parking spot not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> deleteParkingSpot(@PathVariable (value="id" ) UUID id){
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if(parkingSpotModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("parking spot not found");
+        }
+        parkingSpotService.delete(parkingSpotModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("parking spot delete successfully");
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> updateParkingSpot(@PathVariable(value="id") UUID id,
+                                                    @RequestBody @Valid ParkingSpotDTO parkingSpotDTO){
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (parkingSpotModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found");
+        }
+        var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDTO,parkingSpotModel);
+        parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+        parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
+    }
+}
